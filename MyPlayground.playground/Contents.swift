9c23843d33ed9a40f5bdf6e860546eee3ad879df
Nikita <a2143319@yandex.ru>
2020-07-01 22:42:37 +0500

    lesson2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+//Задание 1.
+
+func isNumberEven (number: Int) -> Bool{
+    return number % 2 == 0
+}
+
+//Задание 2.
+
+func isNumberDivisibleThree (number: Int) -> Bool{
+    return number % 3 == 0
+}
+
+// Задание 3.
+
+var myArray: [Int] = []
+
+var a = 0
+
+for _ in stride(from: 1, to: 101, by: 1) {
+    myArray.append(a+1)
+    a += 1
+}
+
+//Задание 4.
+
+myArray.removeAll {isNumberEven(number: $0) || isNumberDivisibleThree(number: $0)}
+
+
+
+
