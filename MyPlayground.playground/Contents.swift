6cffeb355a1795174fcd23f45e92a32a71caff3c
Nikita <a2143319@yandex.ru>
2020-07-12 16:31:11 +0500

    ООП. Домашнее задание. Дмитриев Никита

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,24 +1,91 @@
 import Foundation
 
-class Car {
+enum Position {
     
-    var quantityDoors: Int
-    var yearRelease: Int
-    var enginePower: Int
-    var color: String
+    case right, left
+}
+
+enum Volume {
+    case big, medium, little
+}
+
+protocol AllCars {
     
-    func repaint(newColor: String) -> String {
-        
+    var positionWheel: Position { get set }
+    var doorsCount: Int { get set }
+    var color: String { get set }
+    
+    func drive()
+}
+
+extension AllCars {
+    
+    mutating func repaint(newColor: String) -> String {
         color = newColor
         return newColor
     }
+}
+
+extension AllCars {
     
+    mutating func changePositionWheel(newPosition: Position) -> Position {
+        positionWheel = newPosition
+        return newPosition
+    }
+}
+
+extension AllCars {
     
-    init(doors: Int, year: Int, power: Int, color: String) {
-        self.quantityDoors = doors
-        self.yearRelease = year
-        self.enginePower = power
+    func drive(line: Position) {
+        print("начинаем движение в \(line)")
+    }
+}
+
+class Cars: AllCars {
+    
+    var positionWheel: Position
+    
+    var doorsCount: Int
+    
+    var color: String
+    
+    func drive() {
+        print("Машина поехала")
+    }
+    
+    init(wheel: Position, door: Int, color: String) {
+        self.positionWheel = wheel
+        self.doorsCount = door
         self.color = color
+    }
+}
+
+class TrunkCar: AllCars {
+    
+    func drive() {
+        print("поехали в неизвестном направлении")
+    }
+    
+    var positionWheel: Position
+    
+    var doorsCount: Int
+    
+    var color: String
+    
+    var maxVolume: Volume
+    
+    init(wheel: Position, door: Int, color: String, volume: Volume) {
+        self.positionWheel = wheel
+        self.doorsCount = door
+        self.color = color
+        self.maxVolume = volume
+    }
+}
+
+extension TrunkCar: CustomStringConvertible {
+    
+    var description: String {
+        return "\(color), \(maxVolume), \(doorsCount), \(positionWheel)"
     }
 }
 
@@ -26,100 +93,50 @@ enum Speed {
     case fast, superFast
 }
 
-
-class SportCar: Car {
+class SportCar: AllCars {
     
-    var brand: String
-    var torque: Int
+    func drive() {
+        print("поехали в неизвестном направлении")
+    }
+    
+    var positionWheel: Position
+    
+    var doorsCount: Int
+    
+    var color: String
+    
     var maxSpeed: Speed
-
-    func speedMax(_: Speed) {
-        
-        var carSpeed: Int
-        
-        switch maxSpeed {
-        case .fast:
-            carSpeed = 300
     
-        case .superFast:
-            carSpeed = 400
-        }
-        print("Максимальная скорость твоего автомобиля \(carSpeed)")
-    }
-    
-    override func repaint(newColor: String) -> String {
-        
-        if color == "Red"{
-            print("отличный цвет для спортивной машины")
-        }else{
-            color = newColor
-        }
-        return newColor
-    }
-    
-    init(brand: String, torque: Int, speed: Speed) {
+    init(wheel: Position, door: Int, color: String, speed: Speed) {
+        self.positionWheel = wheel
+        self.doorsCount = door
+        self.color = color
         self.maxSpeed = speed
-        self.torque = torque
-        self.brand = brand
-        super.init(doors: 2, year: 2010, power: 500, color: "Red")  // не очень понимаю почему тут обязательно заполнять реальными значениями doors year power и тд, они будут использованы если init Car каким-то образом не сработает? Мы же всегда сначала Car создадим, а потом SportCar
     }
 }
 
-enum Volume {
-    case big, medium, little
-}
-
-class TrunkCar: Car {
+extension SportCar: CustomStringConvertible {
     
-    var brand: String
-    var maxVolume: Volume
-    var trailer: Bool
-    
-    func volumeMax(_: Volume) {
-        
-        var carVolume: Int
-        
-        switch maxVolume {
-        case .big:
-            carVolume = 10
-    
-        case .medium:
-            carVolume = 5
-        
-        case .little:
-            carVolume = 2
-        }
-        print("Максимальная грузоподъемность твоего автомобиля: \(carVolume) ")
-    }
-    
-    override func repaint(newColor: String) -> String {
-           
-           if color == "Red"{
-               print("Слишком броско для твоей машины")
-           }else{
-               color = newColor
-           }
-           return newColor
-       }
-    
-    init(brand: String, volume: Volume, trailer: Bool) {
-        self.maxVolume = volume
-        self.trailer = trailer
-        self.brand = brand
-        super.init(doors: 4, year: 2000, power: 200, color: "Black")
+    var description: String {
+        return "\(color), \(maxSpeed), \(doorsCount), \(positionWheel)"
     }
 }
 
-var mainCar = Car(doors: 2, year: 2008, power: 300, color: "grey")
-var sportCar = SportCar(brand: "BMW", torque: 500, speed: .fast)
-var trunkCar = TrunkCar(brand: "Volvo", volume: .big, trailer: true)
+var myTrunkCar = TrunkCar(wheel: .left, door: 4, color: "Black", volume: .big)
+myTrunkCar.drive()
+myTrunkCar.drive(line: .left)
+myTrunkCar.description
+myTrunkCar.repaint(newColor: "Blue")
+myTrunkCar.changePositionWheel(newPosition: .right)
+myTrunkCar.description
 
-mainCar.color = "Red"
-sportCar.repaint(newColor: "Green")
-print(mainCar.color)
-trunkCar.repaint(newColor: "Purple") // не могу понять почему нет принта "Слишком броско для твоей машины"
-print(mainCar.color)
+var mySportCar = SportCar(wheel: .left, door: 2, color: "Red", speed: .fast)
+mySportCar.color
+mySportCar.drive()
+mySportCar.changePositionWheel(newPosition: .right)
+mySportCar.drive(line: .left)
+mySportCar.repaint(newColor: "Grey")
+mySportCar.description
 
-sportCar.speedMax(.fast)
-trunkCar.volumeMax(.little)
-
+print("Характеристики спортивного автомобиля:" + mySportCar.description)
+print("Характеристики грузового автомобиля:" + myTrunkCar.description)
